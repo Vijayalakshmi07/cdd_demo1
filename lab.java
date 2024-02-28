3fc8214139c537ea49d58483756843353bb85ca5
Vijayalakshmi07 <108816048+Vijayalakshmi07@users.noreply.github.com>
2024-02-28 11:46:29 +0530

    lab.java

---
+++ lab.java
@@ -0,0 +1,11 @@
+class main
+{    
+    public static void main(String args[])   
+    {            
+        int x = 10, y = 20, a = 5, b = 5, sum, s;    
+        sum=Integer.sum(x, y);  
+        System.out.println("The sum of x and y is: " +sum);    
+        s=Integer.sum(a, b);  
+        System.out.println("The sum of x and y is: " +s);  
+    }    
+} 
